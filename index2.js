70d5393d19b052a4c10a3ec26dc71c1104bbf350
Hind <hind.nayhi@gmail.com>
2022-08-04 17:22:19 -0600

    Refactored the iss_spotter project with Promises.

---
+++ index2.js
@@ -0,0 +1,12 @@
+const { nextISSTimesForMyLocation } = require('./iss_promised');
+
+nextISSTimesForMyLocation()
+  .then((flyTimes) => {
+    for (let time of flyTimes) {
+      const date = new Date(time.risetime * 1000); //when passed a date, the date constructor turns it into current date and local timezone.
+      console.log(`Next pass at ${date} for ${time.duration} seconds!`);
+    }
+  })
+  .catch((error) => {
+    console.log('It didn\'t work: ', error.message); //Message is extracted from body, it's either success or failure.
+  });
---
+++ iss_promised.js
@@ -0,0 +1,31 @@
+const request = require('request-promise-native');
+
+const fetchMyIP = function() {
+  return request('https://api.ipify.org?format=json');
+};
+
+const fetchCoordsByIP = function(body) {
+  const data = JSON.parse(body);
+  const IP = data.ip;
+
+  return request(`http://ipwho.is/${IP}`);
+};
+
+const fetchISSFlyOverTimes = function(body) {
+  const data = JSON.parse(body);
+  const coordinates = { latitude: data.latitude, longitude: data.longitude };
+  return request(`http://api.open-notify.org/iss-pass.json?lat=${coordinates.latitude}&lon=${coordinates.longitude}`);
+};
+
+const nextISSTimesForMyLocation = function() {
+  return fetchMyIP()
+    .then(fetchCoordsByIP)
+    .then(fetchISSFlyOverTimes)
+    .then(body => {
+      const data = JSON.parse(body);
+      const flyTimes = data.response;
+      return flyTimes;
+    });
+};
+
+module.exports = { nextISSTimesForMyLocation };
